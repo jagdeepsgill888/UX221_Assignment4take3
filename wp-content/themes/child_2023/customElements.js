064c87cc27db36a00709d1aeecc3756695497c4a
jagdeepsgill888 <jagdeepsgill888@users.noreply.github.com>
2024-04-02 00:18:58 +0000

    deploy: 110df3b6e5509bcd16999868806b03fbfe292e40

--- wp-content/themes/child_2023/customElements.js
+++ wp-content/themes/child_2023/customElements.js
@@ -5,3 +5,22 @@ class CopyrightYear extends HTMLElement{
 }
 
 customElements.define("x-year", CopyrightYear);
+
+
+//Class JagWasHere extends HTMLElement{
+//	connectCallback(){
+//		this.innerHTML = "Jag was here";
+		
+//	}
+// }
+
+// customElements.define("x-jag", JagWasHere);
+
+
+//class TwoSidedMarket extends HTMLElement{
+//	connectedCallback(){
+//		this.innerHTML = `<a herf="side-a">Side A</a>&nbsp;<a href="side-b">Side B</a>`;
+//	}
+// }
+
+//customElements.define("x-twosided"), (TwoSidedMartet);
