65fb9666c0740c86e218677f54c932803997710d
jagdeepsgill888 <jagdeepsgill888@users.noreply.github.com>
2024-04-09 07:19:14 +0000

    deploy: 84661b12dac4536bfb636a4e67ef84524a21b7fd

--- wp-content/themes/child_2023/customElements.js
+++ wp-content/themes/child_2023/customElements.js
@@ -17,10 +17,10 @@ class RunTrailSpot extends HTMLElement{
 customElements.define("x-trail", RunTrailSpot);
 
 
-//class TwoSidedMarket extends HTMLElement{
-//	connectedCallback(){
-//		this.innerHTML = `<a herf="side-a">Side A</a>&nbsp;<a href="side-b">Side B</a>`;
-//	}
-// }
+class TwoSidedMarket extends HTMLElement{
+	connectedCallback(){
+		this.innerHTML = `<a href="/scope:0.4785388568406725/products/asics-gel-nimbus-26/">Asics Gel Nimbus 26</a>&nbsp;<a href="/scope:0.4785388568406725/products/nb-xc-5k-v6/">NB-XC</a>`;
+	}
+ }
 
-//customElements.define("x-twosided"), (TwoSidedMartet);
+customElements.define("x-twosided", TwoSidedMarket);
